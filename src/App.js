0cdbfaf9260cc7f755ec6e29da51ca8a3fab5287
rudicarrilloypr <125106965+rudicarrilloypr@users.noreply.github.com>
2023-07-19 12:10:35 -0600

    Add navigation routes

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import CategoriesPage from './pages/CategoriesPage';
+import Navigation from './components/Navigation'; // Asegúrate de importar Navigation
 
 const App = () => (
   <Router>
+    <Navigation />
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/categories" element={<CategoriesPage />} />
