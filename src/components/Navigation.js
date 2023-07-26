0d1958f43613d80d0bfa60cbf329b4ca911d4c56
rudicarrilloypr <125106965+rudicarrilloypr@users.noreply.github.com>
2023-07-26 03:21:18 -0600

    Add user icon

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,18 @@
-// Navigation.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import PersonIcon from '@mui/icons-material/Person';
 
 const Navigation = () => (
   <nav className="navbar-container">
-    <h1 className="Bookstore-CMS">Bookstore CMS</h1>
-    <Link to="/" className="books">BOOKS</Link>
-    <Link to="/categories" className="categories">CATEGORIES</Link>
+    <div className="navbar-content">
+      <h1 className="Bookstore-CMS">Bookstore CMS</h1>
+      <Link to="/" className="books">BOOKS</Link>
+      <Link to="/categories" className="categories">CATEGORIES</Link>
+    </div>
+    <div className="navbar-icon">
+      <PersonIcon />
+    </div>
   </nav>
 );
 
